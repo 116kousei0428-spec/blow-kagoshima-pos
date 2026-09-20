a5bb79101d819992437d61a21f09f2b4c8cc661b
116kousei0428-spec <116kousei0428@gmail.com>
2026-09-20 21:47:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blow-kagoshima-pos-icon-20260920d';
+const CACHE_NAME = 'blow-kagoshima-pos-weekly-closing-20260920e';
 const STATIC_ASSETS = ['./index.html','./manifest.json','./favicon.ico','./icon-16.png','./icon-32.png','./icon-48.png','./icon-64.png','./icon-128.png','./icon-192.png','./icon-256.png','./icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))); });
 self.addEventListener('activate', event => { event.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))), self.clients.claim()])); });
